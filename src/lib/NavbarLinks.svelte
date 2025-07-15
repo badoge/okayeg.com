37ae63adff37b632e2f5229143c171e10af34f6a
badoge <18620902+badoge@users.noreply.github.com>
2025-07-16 03:45:34 +0400

    navbar

--- src/lib/NavbarLinks.svelte
+++ src/lib/NavbarLinks.svelte
@@ -26,50 +26,294 @@
   const route = $derived(page.route.id);
 
   const miscPages = ["/api", "/bounty", "/clips", "/mod", "/recap", "/stream", "/trivia"];
+
+  let openDropdown1 = $state(false);
+  let openDropdown2 = $state(false);
 </script>
 
-<li><a class={route === "/" ? "bg-accent text-accent-content" : ""} href="/"><IcBaselineInfo />About</a></li>
-<li><a class={route === "/commands" ? "bg-accent text-accent-content" : ""} href="/commands"><IcBaselineEquals />Commands</a></li>
-<li><a class={route === "/leaderboard" ? "bg-accent text-accent-content" : ""} href="/leaderboard"><IconoirLeaderboardStar />Leaderboard</a></li>
-<li><a class={route === "/stocks" ? "bg-accent text-accent-content" : ""} href="/stocks"><IcOutlineTrendingUp />Stocks</a></li>
-<li><a class={route === "/market" ? "bg-accent text-accent-content" : ""} href="/market"><IcBaselineStorefront />NFE market</a></li>
-<li><a class={route?.startsWith("/profile") ? "bg-accent text-accent-content" : ""} href="/profile"><IcBaselineAccountCircle />Profile</a></li>
+<li>
+  <a
+    class={route === "/" ? "bg-accent text-accent-content" : ""}
+    href="/"
+    onclick={() => {
+      openDropdown1 = false;
+      openDropdown2 = false;
+    }}
+  >
+    <IcBaselineInfo />About
+  </a>
+</li>
+<li>
+  <a
+    class={route === "/commands" ? "bg-accent text-accent-content" : ""}
+    href="/commands"
+    onclick={() => {
+      openDropdown1 = false;
+      openDropdown2 = false;
+    }}
+  >
+    <IcBaselineEquals />Commands
+  </a>
+</li>
+<li>
+  <a
+    class={route === "/leaderboard" ? "bg-accent text-accent-content" : ""}
+    href="/leaderboard"
+    onclick={() => {
+      openDropdown1 = false;
+      openDropdown2 = false;
+    }}
+  >
+    <IconoirLeaderboardStar />Leaderboard
+  </a>
+</li>
+<li>
+  <a
+    class={route === "/stocks" ? "bg-accent text-accent-content" : ""}
+    href="/stocks"
+    onclick={() => {
+      openDropdown1 = false;
+      openDropdown2 = false;
+    }}
+  >
+    <IcOutlineTrendingUp />Stocks
+  </a>
+</li>
+<li>
+  <a
+    class={route === "/market" ? "bg-accent text-accent-content" : ""}
+    href="/market"
+    onclick={() => {
+      openDropdown1 = false;
+      openDropdown2 = false;
+    }}
+  >
+    <IcBaselineStorefront />NFE market
+  </a>
+</li>
+<li>
+  <a
+    class={route?.startsWith("/profile") ? "bg-accent text-accent-content" : ""}
+    href="/profile"
+    onclick={() => {
+      openDropdown1 = false;
+      openDropdown2 = false;
+    }}
+  >
+    <IcBaselineAccountCircle />Profile
+  </a>
+</li>
 <div class="divider divider-horizontal divider-accent m-0"></div>
 <li>
-  <details>
-    <summary class={route?.startsWith("/twitch") ? "bg-accent text-accent-content" : ""}><MdiTwitch />Twitch data</summary>
+  <details bind:open={openDropdown1}>
+    <summary
+      class={route?.startsWith("/twitch") ? "bg-accent text-accent-content" : ""}
+      onclick={() => {
+        openDropdown2 = false;
+      }}
+    >
+      <MdiTwitch />Twitch data</summary
+    >
     <ul class="bg-accent text-secondary-content rounded-box w-55 p-2 z-1 mt-2 shadow-sm">
-      <li><a class={route === "/twitch" ? "bg-neutral text-neutral-content" : ""} href="/twitch"><strong>Twitch related pages</strong></a></li>
+      <li>
+        <a
+          class={route === "/twitch" ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+        >
+          <strong>Twitch related pages</strong>
+        </a>
+      </li>
       <div class="divider divider-neutral m-0"></div>
-      <li><a class={route === "/twitch/emote" ? "bg-neutral text-neutral-content" : ""} href="/twitch/emote"><MingcuteEmojiLine />Emote info</a></li>
-      <li><a class={route === "/twitch/emotes" ? "bg-neutral text-neutral-content" : ""} href="/twitch/emotes"><IcOutlineEmojiEmotions />Channel emotes</a></li>
-      <li><a class={route === "/twitch/global/emotes" ? "bg-neutral text-neutral-content" : ""} href="/twitch/global/emotes"><IcBaselinePublic />Global emotes</a></li>
-      <!-- <li><a class={route === "/twitch/stats" ? "bg-neutral text-neutral-content" : ""} href="/twitch/stats"><IcBaselineQueryStats />Donk stats</a></li> -->
-      <li><a class={route === "/twitch/subs" ? "bg-neutral text-neutral-content" : ""} href="/twitch/subs"><IcBaselineAttachMoney />Local sub prices</a></li>
-      <li><a class={route === "/twitch/time" ? "bg-neutral text-neutral-content" : ""} href="/twitch/time"><IcBaselineHistory />VOD time</a></li>
-      <li><a class={route?.startsWith("/twitch/user") ? "bg-neutral text-neutral-content" : ""} href="/twitch/user"><IcBaselinePerson />User info</a></li>
+      <li>
+        <a
+          class={route?.startsWith("/twitch/emote/") || route === "/twitch/emote" ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch/emote"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+        >
+          <MingcuteEmojiLine />Emote info
+        </a>
+      </li>
+      <li>
+        <a
+          class={route?.startsWith("/twitch/emotes/") || route === "/twitch/emotes" ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch/emotes"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+        >
+          <IcOutlineEmojiEmotions />Channel emotes</a
+        >
+      </li>
+      <li>
+        <a
+          class={route === "/twitch/global/emotes" ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch/global/emotes"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+        >
+          <IcBaselinePublic />Global emotes
+        </a>
+      </li>
+      <!-- <li>
+        <a
+          class={route === "/twitch/stats" ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch/stats"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+        >
+          <IcBaselineQueryStats />Donk stats
+        </a>
+      </li> -->
+      <li>
+        <a
+          class={route === "/twitch/subs" ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch/subs"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+          ><IcBaselineAttachMoney />
+          Local sub prices
+        </a>
+      </li>
+      <li>
+        <a
+          class={route === "/twitch/time" ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch/time"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+        >
+          <IcBaselineHistory />VOD time
+        </a>
+      </li>
+      <li>
+        <a
+          class={route?.startsWith("/twitch/user") ? "bg-neutral text-neutral-content" : ""}
+          href="/twitch/user"
+          onclick={() => {
+            openDropdown1 = false;
+          }}
+        >
+          <IcBaselinePerson />User info
+        </a>
+      </li>
     </ul>
   </details>
 </li>
 <li>
-  <details>
-    <summary class={miscPages.includes(route) ? "bg-accent text-accent-content" : ""}><IcBaselineCategory />Misc. pages</summary>
+  <details bind:open={openDropdown2}>
+    <summary
+      class={miscPages.includes(route) ? "bg-accent text-accent-content" : ""}
+      onclick={() => {
+        openDropdown1 = false;
+      }}
+    >
+      <IcBaselineCategory />Misc. pages</summary
+    >
     <ul class="bg-accent text-secondary-content rounded-box w-55 p-2 z-1 mt-2 shadow-sm">
       <li><a href="/"><strong>Miscellaneous pages</strong></a></li>
       <div class="divider divider-neutral m-0"></div>
-      <li><a class={route === "/api" ? "bg-neutral text-neutral-content" : ""} href="/api"><IcBaselineApi />API</a></li>
-      <li><a class={route === "/bounty" ? "bg-neutral text-neutral-content" : ""} href="/bounty"><IcBaselineDiamond />Bounty</a></li>
-      <li><a class={route === "/clips" ? "bg-neutral text-neutral-content" : ""} href="/clips"><IcBaselineMovie />Clips</a></li>
-      <li><a class={route === "/mod" ? "bg-neutral text-neutral-content" : ""} href="/mod"><MingcuteSwordFill />Mod</a></li>
-      <li><a class={route === "/recap" ? "bg-neutral text-neutral-content" : ""} href="/recap"><IcBaselineHistory />2023 Recap</a></li>
-      <li><a class={route === "/stream" ? "bg-neutral text-neutral-content" : ""} href="/stream"><IcBaselineLiveTv />Stream</a></li>
-      <li><a class={route === "/trivia" ? "bg-neutral text-neutral-content" : ""} href="/trivia"><IcBaselineQuiz />Trivia</a></li>
+      <li>
+        <a
+          class={route === "/api" ? "bg-neutral text-neutral-content" : ""}
+          href="/api"
+          onclick={() => {
+            openDropdown2 = false;
+          }}
+        >
+          <IcBaselineApi />API
+        </a>
+      </li>
+      <li>
+        <a
+          class={route === "/bounty" ? "bg-neutral text-neutral-content" : ""}
+          href="/bounty"
+          onclick={() => {
+            openDropdown2 = false;
+          }}
+        >
+          <IcBaselineDiamond />Bounty
+        </a>
+      </li>
+      <li>
+        <a
+          class={route === "/clips" ? "bg-neutral text-neutral-content" : ""}
+          href="/clips"
+          onclick={() => {
+            openDropdown2 = false;
+          }}
+        >
+          <IcBaselineMovie />Clips
+        </a>
+      </li>
+      <li>
+        <a
+          class={route === "/mod" ? "bg-neutral text-neutral-content" : ""}
+          href="/mod"
+          onclick={() => {
+            openDropdown2 = false;
+          }}
+        >
+          <MingcuteSwordFill />Mod
+        </a>
+      </li>
+      <li>
+        <a
+          class={route === "/recap" ? "bg-neutral text-neutral-content" : ""}
+          href="/recap"
+          onclick={() => {
+            openDropdown2 = false;
+          }}
+        >
+          <IcBaselineHistory />2023 Recap
+        </a>
+      </li>
+      <li>
+        <a
+          class={route === "/stream" ? "bg-neutral text-neutral-content" : ""}
+          href="/stream"
+          onclick={() => {
+            openDropdown2 = false;
+          }}
+        >
+          <IcBaselineLiveTv />Stream
+        </a>
+      </li>
+      <li>
+        <a
+          class={route === "/trivia" ? "bg-neutral text-neutral-content" : ""}
+          href="/trivia"
+          onclick={() => {
+            openDropdown2 = false;
+          }}
+        >
+          <IcBaselineQuiz />Trivia
+        </a>
+      </li>
     </ul>
   </details>
 </li>
 <div class="divider divider-horizontal divider-accent m-0"></div>
 
-<li><a class={route === "/egdle" ? "bg-accent text-accent-content" : ""} href="/egdle"><IcBaselineEgg />Egdle</a></li>
+<li>
+  <a
+    class={route === "/egdle" ? "bg-accent text-accent-content" : ""}
+    href="/egdle"
+    onclick={() => {
+      openDropdown1 = false;
+      openDropdown2 = false;
+    }}
+  >
+    <IcBaselineEgg />Egdle
+  </a>
+</li>
 
 <style>
   li {
