8faec9ba76b52529469d9ec120be49bceaedc01c
badoge <18620902+badoge@users.noreply.github.com>
2025-10-06 00:04:14 +0400

    fix theme switcher + update stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,6 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 
-export default {
-  kit: {
-    adapter: adapter({
-      // See below for an explanation of these options
-      config: undefined,
-      platformProxy: {
-        configPath: undefined,
-        environment: undefined,
-        persist: undefined,
-      },
-      fallback: "plaintext",
-      routes: {
-        include: ["/*"],
-        exclude: ["<all>"],
-      },
-    }),
-  },
-};
+/** @type {import('@sveltejs/kit').Config} */
+const config = { kit: { adapter: adapter() } };
+
+export default config;
